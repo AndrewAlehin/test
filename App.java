6c70953f9785f812adce5978ab303e8311288bbd
Andrew Alehin <andrewaleh@mail.ru>
2021-05-09 10:39:31 +0300

    test
    (cherry picked from commit 0bb61de9b0f054b2e6ab571751ef3cebffc714c1)

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
  class App {
      public static void main(String[] args) {
          System.out.println("Hello world!!!");
-         System.out.println("HELLO world");
+         System.out.println("Helo world WORLD!!!");
          System.out.println("Hello world!!!");
      }
  }
